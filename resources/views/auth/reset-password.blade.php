d6619bbb48843efaf98f3fa0c7495b7e91c2cad0
mstkm <99125333+mstkm@users.noreply.github.com>
2023-03-25 14:13:34 +0700

    add reset password feature

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -9,17 +9,32 @@
 </head>
 <body>
   <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
-    <form action="/forgot-password" method="post" class="col-lg-4">
+    <form action="/reset-password" method="post" class="col-lg-4">
       @csrf
       <img src="{{ url('/frontend/images/nomads_logo/drawable-xhdpi/logo_nomads.png') }}" alt="nomads_logo" class="img-fluid mx-auto d-block mb-2" width="200">
-      <h1 class="text-center mb-3">Forgot Password</h1>
+      <h1 class="text-center mb-3">Rest Password</h1>
+      @if(session()->has('success'))
+        {{ session('status') }}
+      @endif
+
+      <!-- Password Reset Token -->
+      <input type="hidden" name="token" value="{{ $token }}">
+
       <div class="mb-3">
         <label for="email" class="form-label">Email</label>
         <input name="email" type="email" class="form-control" id="email" placeholder="Input your email">
       </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input name="password" type="password" class="form-control" id="password" placeholder="Input your password">
+      </div>
+      <div class="mb-3">
+        <label for="password_confirmation" class="form-label">Confirm Password</label>
+        <input name="password_confirmation" type="password" class="form-control" id="password_confirmation" placeholder="Input your confirm password">
+      </div>
 
       <div class="mt-4">
-        <button class="btn btn-primary w-100" type="submit">Send Email Reset Password Link</button>
+        <button class="btn btn-primary w-100" type="submit">Reset Password</button>
       </div>
     </form>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\DetailController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Client\CheckoutController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +34,16 @@ Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/logout', [LoginController::class, 'logoutNotFound']);
+
+// Forgot / Reset Password
+Route::get('/forgot-password', [ForgotPasswordController::class, 'index'])->name('forgot-password');
+Route::post('/forgot-password', [ForgotPasswordController::class, 'requestLink'])->name('request-link');
+
+Route::get('/reset-password/{token}', function (string $token) {
+  return view('auth.reset-password', ['token' => $token]);
+})->middleware('guest')->name('password.reset');
+
+Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword'])->middleware('guest')->name('password.update');
 
 // Detail
 Route::get('/detail', [DetailController::class, 'index'])->name('detail');
