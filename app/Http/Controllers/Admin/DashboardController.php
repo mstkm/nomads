fea5f5ee03e64e6373199ad9f6a2a6021d75d787
mstkm <mustakimmugi@gmail.com>
2023-04-13 14:04:34 +0700

    dashboard information

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\TravelPackage;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request) {
-      return view('pages.admin.dashboard');
+      $travel_pakages = TravelPackage::all();
+      $transactions = Transaction::all();
+      $transactions_pending = Transaction::where('transaction_status', 'PENDING');
+      $transactions_success = Transaction::where('transaction_status', 'SUCCESS');
+
+
+      return view('pages.admin.dashboard', [
+        'travel_packages' => $travel_pakages,
+        'transactions' => $transactions,
+        'transactions_pending' => $transactions_pending,
+        'transactions_success' => $transactions_success,
+      ]);
     }
 }
